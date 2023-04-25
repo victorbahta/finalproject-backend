8573902e5976e7ebec584ae6d2161d73ff05cc51
victorbahtas <victorbahta@Victors-MacBook-Air.local>
2023-04-25 10:59:54 -0500

    account repo updated

--- src/main/java/com/finalproject/finalproject/repo/AccountRepository.java
+++ src/main/java/com/finalproject/finalproject/repo/AccountRepository.java
@@ -3,6 +3,9 @@ package com.finalproject.finalproject.repo;
 import com.finalproject.finalproject.domain.Accounts;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends CrudRepository<Accounts, Long> {
     Accounts save(Accounts account);
+    List<Accounts> findAll();
 }
